1e69c673cd0fcaf90d27b4d603666e064d722389
Ak. Malcolm <akechsolo44-cpu>
2026-02-24 15:38:42 +0700

    code5

---
+++ LargestNo.py
@@ -0,0 +1,9 @@
+num1 = float(input("Enter number 1: "))
+num2 = float(input("Enter number 2: "))
+
+if num1 > num2:
+    print(f"The largest number is: {num1}")
+elif num2 > num2:
+    print(f"The largest number is: {num2}")
+else:
+    print("Both numbers are equal")
