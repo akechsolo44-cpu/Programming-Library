21d5465ecdc395e2763373f9c1818902fd1341a2
Ak. Malcolm <akechsolo44-cpu>
2026-03-08 18:51:11 +0700

    pycommit

---
+++ DiscountConversion.py
@@ -0,0 +1,9 @@
+km = float(input("Enter distance in kilometers: "))
+
+meters = km * 1000
+centimeters = meters * 100
+miles = meters * 0.621371
+
+print("Meters:", meters)
+print("Centimeters:", centimeters)
+print("Miles:", miles)
