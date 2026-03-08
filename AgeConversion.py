1735adf40db26d9e662a53dd2ee8fa11cffa80ca
Ak. Malcolm <akechsolo44-cpu>
2026-03-08 18:49:39 +0700

    pycommit

---
+++ AgeConversion.py
@@ -0,0 +1,9 @@
+years = int(input("Enter age in years: "))
+
+months = years * 12
+weeks = years * 54
+days = years * 365/366
+
+print("Months:", months)
+print("Weeks:", weeks)
+print("Days:", days)
