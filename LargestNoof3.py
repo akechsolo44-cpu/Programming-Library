b8b49492687c746e57ebc69ed5887253960f88c5
Ak. Malcolm <akechsolo44-cpu>
2026-04-11 19:03:38 +0700

    mycommit

---
+++ LargestNoof3.py
@@ -0,0 +1,7 @@
+p, q, r, = 8, -1, 100
+if p > q and p > r:
+    print("Largest number is: ", p)
+elif q > p and q > r:
+    print("Largest number is: ", q)
+else:
+    print("Largest number is: ", r)
