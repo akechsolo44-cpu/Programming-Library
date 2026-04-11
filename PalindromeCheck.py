0e7af9541c86d7b725514744d7b6d54deabfb032
Ak. Malcolm <akechsolo44-cpu>
2026-04-11 19:05:16 +0700

    mycommit

---
+++ PalindromeCheck.py
@@ -0,0 +1,11 @@
+words = ["Ak", "Malolm", "Xtra"]
+
+is_palindrome = True
+
+if words[0] != words[-1]:
+    is_palindrome = False
+
+if is_palindrome:
+    print("Palindrome")
+else:
+    print("Not palindrome")
