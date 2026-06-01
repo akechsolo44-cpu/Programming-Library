9f4f8d96e4c86df97bb6dab8de8c64a3272b6d12
Ak.MalcolmXtra <akech.solo44@gmail.com>
2026-06-01 13:46:45 +0700

    pycommit

---
+++ InvertedPyramid.py
@@ -0,0 +1,14 @@
+n = 5
+
+for i in range(n):
+    
+    for j in range(i):
+        print("-", end="")
+    
+    for k in range(2 * (n - i) - 1):
+        print("*", end="")
+    
+    for j in range(i):
+        print("-", end="")
+    
+    print()
