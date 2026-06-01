1f4bf61ec7dfd96cb1e080efb3eb9c9fe61e7f63
Ak.MalcolmXtra <akech.solo44@gmail.com>
2026-06-01 13:40:47 +0700

    pycommit

---
+++ DiamondPattern.py
@@ -0,0 +1,10 @@
+n = 5
+mid = n // 2
+
+for i in range(n):
+    for j in range(n):
+        if abs(i - mid) + abs(j - mid) <= mid:
+            print("*", end=" ")
+        else:
+            print("-", end=" ")
+    print()
